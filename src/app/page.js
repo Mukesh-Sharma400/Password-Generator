de1f23a43c716073b3024036d49740b30a4ea5a0
Mukesh Sharma <83642159+Mukesh-Sharma400@users.noreply.github.com>
2023-10-07 13:16:45 +0530

    Generating and Coping Password

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,21 @@
 "use client";
 
-import { useState } from "react";
 import styled from "styled-components";
+import { Tooltip } from "@mui/material";
+import { useRef, useState } from "react";
 import Slider from "@mui/material/Slider";
-import Rating from "@mui/material/Rating";
-import CropPortraitIcon from "@mui/icons-material/CropPortrait";
+import MuiAlert from "@mui/material/Alert";
+import Snackbar from "@mui/material/Snackbar";
+import CustomCheckbox from "./CustomCheckbox";
 import ContentCopyRoundedIcon from "@mui/icons-material/ContentCopyRounded";
 import ArrowForwardRoundedIcon from "@mui/icons-material/ArrowForwardRounded";
-import CustomCheckbox from "./CustomCheckbox";
 
 export default function Home() {
-  const [value, setValue] = useState(7);
+  const [password, setPassword] = useState("");
+  const [value, setValue] = useState(10);
+  const passwordRef = useRef(null);
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState("");
 
   const handleSliderChange = (event, newValue) => {
     setValue(newValue);
@@ -20,62 +25,121 @@ export default function Home() {
     console.log("Checkbox checked:", isChecked);
   };
 
+  function generateRandomPassword(length) {
+    const charset =
+      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+=";
+    let password = "";
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * charset.length);
+      password += charset.charAt(randomIndex);
+    }
+    return password;
+  }
+
+  const handleGeneratePassword = () => {
+    const newPassword = generateRandomPassword(value);
+    setPassword(newPassword);
+  };
+
+  const handleSnackbarOpen = (message) => {
+    setSnackbarMessage(message);
+    setSnackbarOpen(true);
+  };
+
+  const handleSnackbarClose = () => {
+    setSnackbarOpen(false);
+  };
+
+  const handleCopyPassword = () => {
+    if (passwordRef.current) {
+      passwordRef.current.select();
+      document.execCommand("copy");
+      window.getSelection().removeAllRanges();
+      handleSnackbarOpen("Password Copied to Clipboard!");
+    }
+  };
+
   return (
-    <DisplayWrapper>
-      <ContentWrapper>
-        <Heading>Password Generator</Heading>
-        <PasswordWrapper>
-          <Password>PTx1f5DaFX</Password>
-          <CopyButton />
-        </PasswordWrapper>
-        <GeneratorWrapper>
-          <CharLengthWrapper>
-            <CharLengthText>Character Length</CharLengthText>
-            <CharLengthNum>{value}</CharLengthNum>
-          </CharLengthWrapper>
-          <StyledSlider
-            value={typeof value === "number" ? value : 0}
-            onChange={handleSliderChange}
-            step={1}
-            min={0}
-            max={10}
-          />
-          <CheckboxWrapper>
-            <CustomCheckbox
-              label="Include Uppercase Letters"
-              onChange={handleCheckboxChange}
+    <>
+      <DisplayWrapper>
+        <ContentWrapper>
+          <Heading>Password Generator</Heading>
+          <PasswordWrapper>
+            <Password
+              type="text"
+              value={password}
+              readOnly
+              ref={passwordRef}
+              placeholder="PTx1f5DaFX"
             />
-            <CustomCheckbox
-              label="Include Lowercase Letters"
-              onChange={handleCheckboxChange}
+            <Tooltip title="Click to Copy Password" placement="top">
+              <CopyButton onClick={handleCopyPassword} />
+            </Tooltip>
+          </PasswordWrapper>
+          <GeneratorWrapper>
+            <CharLengthWrapper>
+              <CharLengthText>Character Length</CharLengthText>
+              <CharLengthNum>{value}</CharLengthNum>
+            </CharLengthWrapper>
+            <StyledSlider
+              value={typeof value === "number" ? value : 0}
+              onChange={handleSliderChange}
+              step={1}
+              min={5}
+              max={15}
             />
-            <CustomCheckbox
-              label="Include Numbers"
-              onChange={handleCheckboxChange}
-            />
-            <CustomCheckbox
-              label="Include Symbols"
-              onChange={handleCheckboxChange}
-            />
-          </CheckboxWrapper>
-          <StrengthWrapper>
-            <StrengthText>Strength</StrengthText>
-            <StrengthValue>
-              <StrengthValueText>Medium</StrengthValueText>
-              <StrengthValueBoxes>
-                <StrengthValueBox />
-                <StrengthValueBox />
-                <StrengthValueBox />
-                <StrengthValueBox />
-              </StrengthValueBoxes>
-            </StrengthValue>
-          </StrengthWrapper>
-          <GenerateButton>
-            Generate <RightArrow fontSize="inherit" />
-          </GenerateButton>
-        </GeneratorWrapper>
-      </ContentWrapper>
-    </DisplayWrapper>
+            <CheckboxWrapper>
+              <CustomCheckbox
+                label="Include Uppercase Letters"
+                onChange={handleCheckboxChange}
+              />
+              <CustomCheckbox
+                label="Include Lowercase Letters"
+                onChange={handleCheckboxChange}
+              />
+              <CustomCheckbox
+                label="Include Numbers"
+                onChange={handleCheckboxChange}
+              />
+              <CustomCheckbox
+                label="Include Symbols"
+                onChange={handleCheckboxChange}
+              />
+            </CheckboxWrapper>
+            <StrengthWrapper>
+              <StrengthText>Strength</StrengthText>
+              <StrengthValue>
+                <StrengthValueText>Medium</StrengthValueText>
+                <StrengthValueBoxes>
+                  <StrengthValueBox filled={true} />
+                  <StrengthValueBox filled={true} />
+                  <StrengthValueBox filled={false} />
+                  <StrengthValueBox filled={false} />
+                </StrengthValueBoxes>
+              </StrengthValue>
+            </StrengthWrapper>
+            <GenerateButton onClick={handleGeneratePassword}>
+              Generate <RightArrow fontSize="inherit" />
+            </GenerateButton>
+          </GeneratorWrapper>
+        </ContentWrapper>
+      </DisplayWrapper>
+      <Snackbar
+        open={snackbarOpen}
+        autoHideDuration={2000} // Adjust the duration as needed
+        onClose={handleSnackbarClose}
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
+      >
+        <MuiAlert
+          elevation={6}
+          variant="filled"
+          severity="success"
+          onClose={handleSnackbarClose}
+        >
+          {snackbarMessage}
+        </MuiAlert>
+      </Snackbar>
+    </>
   );
 }
 
@@ -111,8 +175,13 @@ const PasswordWrapper = styled.div`
   padding: 20px;
 `;
 
-const Password = styled.p`
+const Password = styled.input`
   font-size: 20px;
+  outline: none;
+  border: none;
+  background: transparent;
+  width: 100%;
+  font-family: "JetBrains-Mono-Extra-Bold";
 `;
 
 const CopyButton = styled(ContentCopyRoundedIcon)`
@@ -192,13 +261,6 @@ const CheckboxWrapper = styled.div`
   color: #c8c6cf;
 `;
 
-const IncludeWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 10px;
-  font-size: 13px;
-`;
-
 const StrengthWrapper = styled.div`
   display: flex;
   align-items: center;
@@ -235,17 +297,9 @@ const StrengthValueBoxes = styled.div`
 const StrengthValueBox = styled.div`
   height: 20px;
   width: 8px;
-  border: 2px solid #95949c;
+  border: 2px solid ${(props) => (props.filled ? "#f8ce65" : "#95949c")};
+  background-color: ${(props) => (props.filled ? "#f8ce65" : "transparent")};
 `;
-
-const StyledRating = styled(Rating)({
-  "& .MuiRating-iconEmpty": {
-    color: "#95949c",
-  },
-  "& .MuiRating-iconFilled": {
-    color: "#f8ce65",
-  },
-});
 
 const GenerateButton = styled.button`
   outline: none;
